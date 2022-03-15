0e8c155f31be8b298c19a4f920e6e06fba90094d
suj8 <96431629+suj8@users.noreply.github.com>
2022-03-15 17:49:55 +0545

    Update script.js

--- script.js
+++ script.js
@@ -11,5 +11,29 @@ $(document).ready(function(){
     $('.menu-btn').click(function(){
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
-    })
+    });
+    //owl carousel script
+    $('.carousel') .owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0:{
+                items: 1,
+                nav: false
+            },
+            600:{
+                items: 2,
+                nav: false
+            },
+            1000:{
+                items: 3,
+                nav: false
+            }
+        
+        }
+
+    
+    });
 });
